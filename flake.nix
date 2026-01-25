55bfc4d05bdd1cecf31919bb57603de41b207dda
William Huebner <wilhueb@gmail.com>
2026-01-25 03:13:26 -0500

    add flake.lock

--- flake.nix
+++ flake.nix
@@ -1,44 +1,34 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
 
   outputs =
-    { self, nixpkgs }:
-    let
-      forAllSystems =
-        f:
-        nixpkgs.lib.genAttrs
-          [
-            "x86_64-linux"
-            "aarch64-linux"
-            "x86_64-darwin"
-            "aarch64-darwin"
-          ]
-          (
-            system:
-            f {
-              pkgs = nixpkgs.legacyPackages.${system};
-            }
-          );
-    in
-    {
-      devShells = forAllSystems (
-        { pkgs }:
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      perSystem =
+        { pkgs, ... }:
         {
-          default = pkgs.mkShell {
+          devShells.default = pkgs.mkShell {
             packages = with pkgs; [
               uv
             ];
             env = {
               UV_PYTHON_DOWNLOADS = "never";
               UV_PYTHON = pkgs.python312.interpreter;
-              UV_ENV_FILE = ".env";
             };
             shellHook = ''
               uv sync
               source .venv/bin/activate
             '';
           };
-        }
-      );
+        };
     };
 }
